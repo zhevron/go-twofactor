1fb7e3079b2b8da8043d1d230b1779141f8dd85b
Thomas Lokshall <thomas@lokshall.no>
2014-12-12 08:48:43 +0100

    Update coverage tests for Go <1.4

--- test-coverage.sh
+++ test-coverage.sh
@@ -3,8 +3,13 @@
 echo "mode: set" > acc.out
 fail=0
 
+# Install the old cover tool if version is not 1.4 or higher.
+if [ -n "$TRAVIS_GO_VERSION" ] && [[ "$TRAVIS_GO_VERSION" < "go1.4" ]]; then
+  go get code.google.com/p/go.tools/cmd/cover
+fi
+
 # Standard go tooling behavior is to ignore dirs with leading underscors
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d); do
+for dir in $(find . -maxdepth 10 -not -path './cmd/*' -not -path './.git*' -not -path '*/_*' -type d); do
   if ls $dir/*.go &> /dev/null; then
     go test -v -coverprofile=profile.out $dir -check.v || fail=1
     if [ -f profile.out ]; then
@@ -17,7 +22,6 @@ done
 
 # Failures have incomplete results, so don't send
 if [ -n "$COVERALLS_TOKEN" ] && [ "$fail" -eq 0 ]; then
-  echo "SENDING"
   $HOME/gopath/bin/goveralls -v -coverprofile=acc.out -service travis-ci -repotoken $COVERALLS_TOKEN
 fi
 
